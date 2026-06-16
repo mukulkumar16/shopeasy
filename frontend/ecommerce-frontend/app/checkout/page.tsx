b5bfe50d58acc9cff87c05ebc5220aebdf522103
mukul kumar <mukulkumarofficial5@gmail.com>
2026-06-16 23:00:40 +0530

    checkout change

--- frontend/ecommerce-frontend/app/checkout/page.tsx
+++ frontend/ecommerce-frontend/app/checkout/page.tsx
@@ -1,6 +1,8 @@
 import { Suspense } from "react";
 import CheckoutClient from "../Components/CheckOutClient";
 
+export const dynamic = "force-dynamic";
+
 export default function Page() {
   return (
     <Suspense fallback={<div>Loading checkout...</div>}>
